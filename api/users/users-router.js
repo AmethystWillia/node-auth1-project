c515271ded4dac60194cc24b35864e50b3c98165
Amethyst Willia <zmithic@gmail.com>
2022-02-21 15:51:51 -0600

    Added skeleton to users model and router as well as required users-router in server.js

--- api/users/users-router.js
+++ api/users/users-router.js
@@ -1,3 +1,9 @@
+const express = require('express');
+
+const router = express.Router();
+
+const Users = require('./users-model');
+//const { restricted } = require('../auth/auth-middleware');
 // Require the `restricted` middleware from `auth-middleware.js`. You will need it here!
 
 
@@ -26,3 +32,4 @@
 
 
 // Don't forget to add the router to the `exports` object so it can be required in other modules
+module.exports = router;
